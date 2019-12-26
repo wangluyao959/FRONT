ec47fcf2e0f6fb35e164a36585e13707f949cb21
wangluyao <15649871531@163.com>
2019-12-26 11:20:41 +0800

    directive练习

--- vue-practice/src/directive/part1.js
+++ vue-practice/src/directive/part1.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 
 Vue.directive('focus',{
+  //指令第一次绑定要元素时调用
+  bind:(el)=>{
+    el.value='插入前'
+  },
   //当绑定元素被插入到DOM元素中
   inserted:(el)=>{
+    console.log('el :', el);
     //聚焦元素
-    el.focus()
+    el.focus();
+    el.value='插入到DOM中'
   }
 })
