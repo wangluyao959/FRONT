e3cb4a232e3d3ae488374caeb3549111c823d7d6
wangluyao <15649871531@163.com>
2019-12-27 09:51:46 +0800

    练习directive

--- vue-practice/src/components/directivePractice/index.vue
+++ vue-practice/src/components/directivePractice/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="wrap">
-    <input type="text" v-model="value" v-focus>
+    <input type="text" v-model="direction" v-focus:foo.bar="1===1">
+    <div class="change-color"></div>
+    <p>Scroll down the page</p>
+    <p v-pin:[direction]="200">Stick me 200px from the top of the page</p>
   </div>
 </template>
 <script>
 export default {
   data(){
     return{
-      value:""
+      value:"heihei ",
+      direction:'bottom'
     }
   },
   methods:{
@@ -18,6 +22,11 @@ export default {
 <style lang="scss" scoped>
 .wrap{
   width: 100%;
-  height: 100%
+  height: 100%;
+  .change-color{
+    width: 500px;
+    height: 500px;
+    background-color: #29383d;
+  }
 }
 </style>
--- vue-practice/src/directive/part1.js
+++ vue-practice/src/directive/part1.js
@@ -2,6 +2,21 @@ import Vue from 'vue';
 
 Vue.directive('focus',{
   //指令第一次绑定要元素时调用
+  bind:()=>{
+  },
+  //当绑定元素被插入到DOM元素中
+  inserted:(el,binding)=>{
+    console.log('el :', el);
+    //聚焦元素
+    el.focus();
+    console.log('binding :', binding);
+
+  }
+})
+
+
+Vue.directive('change-color',{
+  //指令第一次绑定要元素时调用
   bind:(el)=>{
     el.value='插入前'
   },
@@ -11,5 +26,47 @@ Vue.directive('focus',{
     //聚焦元素
     el.focus();
     el.value='插入到DOM中'
+  }
+})
+
+
+
+Vue.directive('pin',{
+  //指令第一次绑定要元素时调用
+  bind:(el,binding,vnode)=>{
+    el.style.position='fixed';
+    // let s=(binding.arg==='left' ? 'left' : 'top');
+    el.style[binding.arg]=binding.value+'px';
+    el.style.color='red';
+  },
+  //当绑定元素被插入到DOM元素中
+  inserted:(el)=>{
+    // el.addEventListener('click',()=>{
+    //   console.log('点击1 :', event);
+    // },false);
+    // el.addEventListener('click',()=>{
+    //   console.log('点击2 :', event);
+    // },false);
+    // el.addEventListener('click',()=>{
+    //   console.log('点击3 :', event);
+    // },true)
+    
+
+    //给document添加点击事件，并进行事件委托
+    document.addEventListener('click',()=>{
+      console.log("event",event);
+      if(event.target['className']==='change-color'){
+        event.target.style.backgroundColor='gray'
+      }
+    })
+
+
+
+
+  },
+  unbind(){
+    // el.addEventListener('click',()=>{
+    //   console.log('event :', event);
+    // })
   }
 })
